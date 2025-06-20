102b463f67d522117dadd4ee532d8a005535ac7c
Ryzzen <a.mayaud@hotmail.fr>
2025-06-20 22:32:09 +0200

    [ADD](flakes) test flakes.nix

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   outputs =
     { self, ... }:
     {
-      # Expect the caller to pass pkgs, so nothing defined here
-      # Just expose a function the user will call manually
       lib.importPackages = { pkgs }: import ./default.nix { inherit pkgs; };
     };
 }
